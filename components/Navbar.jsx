f0dbd9cadeff966cf14a3e2b7f4872c00f392082
1ShoukR <rahminshoukoohi@gmail.com>
2022-11-25 22:01:10 -0500

    added background color change on navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,37 +4,38 @@ import "../css/navbar.css"
 
 const Navbar = () => {
 return (
-		<>
-			<nav className="wrapper">
-				<ul className="container">
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/free_consultation">Free Consultation</Link>
-					</li>
-					<li>
-						<Link to="/train">Training Options</Link>
-					</li>
-					<li>
-						<Link to="/compare">Compare</Link>
-					</li>
-					<li>
-						<Link to="/why_rahmin_fit">Why Rahmin Fit</Link>
-					</li>
-					<li>
-						<Link to="/gallery">Gallery</Link>
-					</li>
-					<li>
-						<Link to="/refund_policy">Refund Policy</Link>
-					</li>
-					<li>
-						<Link to="/contact">Contact Me</Link>
-					</li>
-				</ul>
-			</nav>
-		</>
-	);
+	<>
+		<nav className="wrapper">
+			<ul className="container">
+				<li className="linkContainer">
+					<Link to="/">Home</Link>
+				</li>
+				<li className="linkContainer">
+					<Link to="/free_consultation">Free Consultation</Link>
+				</li>
+				<li className="linkContainer">
+					<Link to="/train">Training Options</Link>
+				</li>
+				<li className="linkContainer">
+					<Link to="/compare">Compare</Link>
+				</li>
+				<li className="linkContainer">
+					<Link to="/why_rahmin_fit">Why Rahmin Fit</Link>
+				</li>
+				<li className="linkContainer">
+					
+					<Link to="/gallery"><a href="">Gallery</a></Link>
+				</li>
+				<li className="linkContainer">
+					<Link to="/refund_policy">Refund Policy</Link>
+				</li>
+				<li className="linkContainer">
+					<Link to="/contact">Contact Me</Link>
+				</li>
+			</ul>
+		</nav>
+	</>
+);
 };
 
 export default Navbar;
