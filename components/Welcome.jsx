be1973b429ccfa49f1eabc58410c80838fee20e9
1ShoukR <rahminshoukoohi@gmail.com>
2022-11-28 21:07:28 -0500

    added framer motion for the image carousel on main page

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { motion } from 'framer-motion';
 
 const Welcome = () => {
   return (
-<>
-  <div>
-    <div>
-      <h1>
-        Welcome
-      </h1>
-    </div>
-  </div>
-</>
-  );
+		<>
+			<motion.div
+				animate={{
+					x: 0,
+					backgroundColor: '#000',
+					boxShadow: '10px 10px 0 rgba(0, 0, 0, 0.2)',
+					position: 'fixed',
+					transitionEnd: {
+						display: 'none',
+					},
+				}}
+			/>
+			<div>
+				<div>
+					<h1>EMBRACE YOUR NEW LIFESTYLE</h1>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Welcome;
