73fea0fbe279835003d6316d62bc3e84daf8c869
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-07 23:46:36 -0500

    added instructions on carousel page

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -3,20 +3,25 @@ import { motion } from 'framer-motion';
 import Carousel from 'framer-motion-carousel';
 
 
+// add images of my fat self and other progress pics from when I trained my friends
+
+
+
 const Welcome = () => {
+	
   return (
-		<>
+<>
 		<Carousel>
-			{[1, 2, 3, 4].map((item, i) => (
-					<img draggable="false" src={`./${item}.jpg`} key={i} width="100%" alt="" />
-			))}
 			<div>
 				<div>
 					<h1>EMBRACE YOUR NEW LIFESTYLE</h1>
 				</div>
 			</div>
+			{[1, 2, 3, 4].map((item, i) => (
+					<img draggable="false" src={`./${item}.jpg`} key={i} width="100%" alt="" />
+			))}
 		</Carousel>
-		</>
+</>
 	);
 };
 
