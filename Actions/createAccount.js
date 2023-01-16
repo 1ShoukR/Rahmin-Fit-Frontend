0136ae71eed0b0020dd6ea052e428a17ef6641af
1ShoukR <rahminshoukoohi@gmail.com>
2023-01-16 12:27:58 -0500

    created an createAccount javascript file

--- Actions/createAccount.js
+++ Actions/createAccount.js
@@ -3,5 +3,18 @@ import axios from "axios"
 
 export const sendCreateAccountData = (e) => {
     e.preventDefault()
-    console.log("Hello")
+    console.log(e.target.form[0].value)
+    let input = {
+        firstName: e.target.form[0].value,
+        lastName: e.target.form[1].value,
+        email: e.target.form[2].value,
+        password: e.target.form[3].value,
+    }
+    axios.post('http://localhost:3000/api/account/create-account', input)
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
 } 
